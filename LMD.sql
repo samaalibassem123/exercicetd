2613b28ddb32b780218e31a85233c5713de8a536
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2025-03-13 20:57:55 +0100

    c4

--- LMD.sql
+++ LMD.sql
@@ -1,85 +1,117 @@
--- Insertion dans DIM_Coureur :
-insert into dim_coureur (
-   num_dossard,
-   nom_coureur,
-   code_equipe,
-   code_pays
-) values ( 101,
-           'test',
-           1,
-           33 ),( 102,
-                  'test',
-                  2,
-                  45 ),( 103,
-                         'test',
-                         3,
-                         33 );
+-- Insertion dans DIM_Coureur
+INSERT INTO
+   dim_coureur (num_dossard, nom_coureur, code_equipe, code_pays)
+VALUES
+   (101, 'test', 1, 33);
 
+INSERT INTO
+   dim_coureur (num_dossard, nom_coureur, code_equipe, code_pays)
+VALUES
+   (102, 'test', 2, 45);
 
--- Insertion dans DIM_Equipe:
-insert into dim_equipe (
-   code_equipe,
-   nom_equipe,
-   directeur
-) values ( 1,
-           'test',
-           'test' ),( 2,
-                      'test',
-                      'test' ),( 3,
-                                 'test',
-                                 'test' );
+INSERT INTO
+   dim_coureur (num_dossard, nom_coureur, code_equipe, code_pays)
+VALUES
+   (103, 'test', 3, 33);
 
+-- Insertion dans DIM_Equipe
+INSERT INTO
+   dim_equipe (code_equipe, nom_equipe, directeur)
+VALUES
+   (1, 'test', 'test');
+
+INSERT INTO
+   dim_equipe (code_equipe, nom_equipe, directeur)
+VALUES
+   (2, 'test', 'test');
+
+INSERT INTO
+   dim_equipe (code_equipe, nom_equipe, directeur)
+VALUES
+   (3, 'test', 'test');
 
 -- Insertion dans DIM_Etape
-insert into dim_etape (
-   num_etape,
-   date_etape,
-   kms,
-   ville_depart,
-   ville_arrivee
-) values ( 1,
-           '2024-07-01',
-           198.5,
-           'test',
-           'test' ),( 2,
-                      '2024-07-02',
-                      175.2,
-                      'test',
-                      'test' );
+INSERT INTO
+   dim_etape (
+      num_etape,
+      date_etape,
+      kms,
+      ville_depart,
+      ville_arrivee
+   )
+VALUES
+   (
+      1,
+      TO_DATE ('2024-07-01', 'YYYY-MM-DD'),
+      198.5,
+      'test',
+      'test'
+   );
 
+INSERT INTO
+   dim_etape (
+      num_etape,
+      date_etape,
+      kms,
+      ville_depart,
+      ville_arrivee
+   )
+VALUES
+   (
+      2,
+      TO_DATE ('2024-07-02', 'YYYY-MM-DD'),
+      175.2,
+      'test',
+      'test'
+   );
 
 -- Insertion dans DIM_Edition
-insert into dim_edition (
-   num_edition,
-   date_deb,
-   date_fin
-) values ( 110,
-           '2024-07-01',
-           '2024-07-28' );
-
+INSERT INTO
+   dim_edition (num_edition, date_deb, date_fin)
+VALUES
+   (
+      110,
+      TO_DATE ('2024-07-01', 'YYYY-MM-DD'),
+      TO_DATE ('2024-07-28', 'YYYY-MM-DD')
+   );
 
 -- Insertion dans DIM_Temps
-insert into dim_temps (
-   id_temps,
-   temps_realise
-) values ( 0,
-           '2024-07-01' ),( 1,
-                            '2024-07-01' ),( 2,
-                                             '2024-07-01' );
+INSERT INTO
+   dim_temps (id_temps, temps_realise)
+VALUES
+   (
+      0,
+      TO_TIMESTAMP ('2024-07-01 12:00:00', 'YYYY-MM-DD HH24:MI:SS')
+   );
+
+INSERT INTO
+   dim_temps (id_temps, temps_realise)
+VALUES
+   (
+      1,
+      TO_TIMESTAMP ('2024-07-01 12:15:00', 'YYYY-MM-DD HH24:MI:SS')
+   );
+
+INSERT INTO
+   dim_temps (id_temps, temps_realise)
+VALUES
+   (
+      2,
+      TO_TIMESTAMP ('2024-07-01 12:30:00', 'YYYY-MM-DD HH24:MI:SS')
+   );
 
 -- Insertion dans Faits_Perfomance
-insert into faits_perfomance (
-   id_temps,
-   num_dossard,
-   num_etape,
-   num_edition
-) values ( 0,
-           101,
-           1,
-           110 ),( 1,
-                   102,
-                   1,
-                   110 ),( 2,
-                           103,
-                           1,
-                           110 );
+INSERT INTO
+   faits_perfomance (id_temps, num_dossard, num_etape, num_edition)
+VALUES
+   (0, 101, 1, 110);
+
+INSERT INTO
+   faits_perfomance (id_temps, num_dossard, num_etape, num_edition)
+VALUES
+   (1, 102, 1, 110);
+
+INSERT INTO
+   faits_perfomance (id_temps, num_dossard, num_etape, num_edition)
+VALUES
+   (2, 103, 1, 110);
--- modelisation.sql
+++ modelisation.sql
@@ -1,4 +1,4 @@
-
+--LDD
 --Table DIM_Equipe:
 create table dim_equipe (
    code_equipe int primary key,
@@ -22,16 +22,17 @@ create table dim_edition (
    date_fin    date
 );
 
-select * from table(dbms_xplan.display_cursor(sql_id=>'null', format=>'ALLSTATS LAST'));
-
-
+select *
+  from table ( dbms_xplan.display_cursor(
+   sql_id => 'null',
+   format => 'ALLSTATS LAST'
+) );
 
 --Table DIM_Temps :
 create table dim_temps (
    id_temps      int primary key,
-   temps_realise TIMESTAMP
+   temps_realise timestamp
 );
-
 
 --Table DIM_Coureur 
 create table dim_coureur (
@@ -42,7 +43,6 @@ create table dim_coureur (
    foreign key ( code_equipe )
       references dim_equipe ( code_equipe )
 );
-
 
 --Table de faits Perfomance :
 create table faits_perfomance (
@@ -61,3 +61,69 @@ create table faits_perfomance (
 );
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--LMD
+-- Insertion dans DIM_Coureur
+INSERT INTO dim_coureur (num_dossard, nom_coureur, code_equipe, code_pays)
+VALUES (101, 'test', 1, 33);
+
+INSERT INTO dim_coureur (num_dossard, nom_coureur, code_equipe, code_pays)
+VALUES (102, 'test', 2, 45);
+
+INSERT INTO dim_coureur (num_dossard, nom_coureur, code_equipe, code_pays)
+VALUES (103, 'test', 3, 33);
+
+-- Insertion dans DIM_Equipe
+INSERT INTO dim_equipe (code_equipe, nom_equipe, directeur)
+VALUES (1, 'test', 'test');
+
+INSERT INTO dim_equipe (code_equipe, nom_equipe, directeur)
+VALUES (2, 'test', 'test');
+
+INSERT INTO dim_equipe (code_equipe, nom_equipe, directeur)
+VALUES (3, 'test', 'test');
+
+-- Insertion dans DIM_Etape
+INSERT INTO dim_etape (num_etape, date_etape, kms, ville_depart, ville_arrivee)
+VALUES (1, TO_DATE('2024-07-01','YYYY-MM-DD'), 198.5, 'test', 'test');
+
+INSERT INTO dim_etape (num_etape, date_etape, kms, ville_depart, ville_arrivee)
+VALUES (2, TO_DATE('2024-07-02','YYYY-MM-DD'), 175.2, 'test', 'test');
+
+-- Insertion dans DIM_Edition
+INSERT INTO dim_edition (num_edition, date_deb, date_fin)
+VALUES (110, TO_DATE('2024-07-01','YYYY-MM-DD'), TO_DATE('2024-07-28','YYYY-MM-DD'));
+
+-- Insertion dans DIM_Temps
+INSERT INTO dim_temps (id_temps, temps_realise)
+VALUES (0, TO_TIMESTAMP('2024-07-01 12:00:00','YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO dim_temps (id_temps, temps_realise)
+VALUES (1, TO_TIMESTAMP('2024-07-01 12:15:00','YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO dim_temps (id_temps, temps_realise)
+VALUES (2, TO_TIMESTAMP('2024-07-01 12:30:00','YYYY-MM-DD HH24:MI:SS'));
+
+-- Insertion dans Faits_Perfomance
+INSERT INTO faits_perfomance (id_temps, num_dossard, num_etape, num_edition)
+VALUES (0, 101, 1, 110);
+
+INSERT INTO faits_perfomance (id_temps, num_dossard, num_etape, num_edition)
+VALUES (1, 102, 1, 110);
+
+INSERT INTO faits_perfomance (id_temps, num_dossard, num_etape, num_edition)
+VALUES (2, 103, 1, 110);
